1a13652333fa98182b0a3cb714591b167f0770c0
Thanh.Nhan.CSE2022 <136608139+ThanhNhanCS22@users.noreply.github.com>
2024-03-23 01:25:44 +0700

    Update Zigzag-Conversion.C

--- Problems/6/Zigzag-Conversion.C
+++ Problems/6/Zigzag-Conversion.C
@@ -1,1 +1,40 @@
+char* convert(char* s, int numRows) {
+    if(numRows == 1){
+        return s ; 
+    }
+    int len = strlen(s); 
+    int indexArr[len] ; 
+    char* result = (char*)malloc((len+1)*sizeof(char)) ; 
+    memset(result,0,len+1);
+    int index = 0 ; 
+    int convertSign = 0 ; 
 
+    for(int i = 0 ; i< len ; i++){
+        if(convertSign == 0 ) { 
+            indexArr[i] = index ;  
+            index++ ; 
+
+        }
+        else if (convertSign == 1 ){
+            indexArr[i] = index ; 
+            index -- ; 
+        }
+        if ( index == numRows - 1 ) convertSign = 1 ; 
+        else if (index == 0) convertSign = 0 ;  
+        
+    }
+    index = 0 ;
+    convertSign = 0 ; 
+    while(index <= numRows -1 ) {
+        for (int i = 0 ; i < len ; i++){
+            if(index == indexArr[i]) {
+                result[convertSign] = s[i] ; 
+                convertSign++; 
+
+            }
+        }
+        index++; 
+    }
+    return result ; 
+    
+}
