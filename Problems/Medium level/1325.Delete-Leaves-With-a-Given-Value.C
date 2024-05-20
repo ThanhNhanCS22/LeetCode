e51daa84baf2b03d76d8ce01188ccdc23a9f2f06
Thanh.Nhan.CSE2022 <136608139+ThanhNhanCS22@users.noreply.github.com>
2024-05-20 13:30:09 +0700

    Create 1325.Delete-Leaves-With-a-Given-Value.C
    https://leetcode.com/problems/delete-leaves-with-a-given-value/description/?envType=daily-question&envId=2024-05-17

---
+++ Problems/Medium level/1325.Delete-Leaves-With-a-Given-Value.C
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* removeLeafNodes(struct TreeNode* root, int target) {
+    if  (root == NULL) return NULL ; 
+    
+    root -> left = removeLeafNodes(root->left , target) ;
+    root ->right = removeLeafNodes(root->right , target) ;
+    if  ( root -> left == NULL && root->right == NULL && target == root->val )  {
+        return NULL ;
+           
+    }
+    return root; 
+}
