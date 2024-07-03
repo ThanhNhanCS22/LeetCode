ae1aca931d9a19d4318ca7296556731ec106309a
Thanh.Nhan.CSE2022 <136608139+ThanhNhanCS22@users.noreply.github.com>
2024-07-03 19:20:07 +0700

    Create 538. Convert BST to Greater Tree.C
    https://leetcode.com/problems/convert-bst-to-greater-tree/

---
+++ Problems/Medium level/538. Convert BST to Greater Tree.C
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+void updateKeyInOrderRightTraversal(struct TreeNode* root, int* sumOfTheRight ) {
+    if(!root) return ; 
+    updateKeyInOrderRightTraversal( root->right,  sumOfTheRight ) ; 
+    *sumOfTheRight += root->val; 
+    root->val = *sumOfTheRight  ; 
+    updateKeyInOrderRightTraversal( root->left,  sumOfTheRight ) ;
+} 
+
+struct TreeNode* convertBST(struct TreeNode* root) {
+    int sumOfTheRight = 0 ; 
+    updateKeyInOrderRightTraversal( root, &sumOfTheRight )  ;
+    return root ; 
+}
