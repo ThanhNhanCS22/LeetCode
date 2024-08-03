3adc975a08632dec140d7e2c07277cc4c8039455
Thanh.Nhan.CSE2022 <136608139+ThanhNhanCS22@users.noreply.github.com>
2024-08-03 19:15:54 +0700

    Update 88.Merge-Sorted-Array.C
    https://leetcode.com/problems/merge-sorted-array/?envType=study-plan-v2&envId=top-interview-150

--- Problems/Easy level/88.Merge-Sorted-Array.C
+++ Problems/Easy level/88.Merge-Sorted-Array.C
@@ -1,38 +1,21 @@
-void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+void merge(int* nums1, int nums1Size, int n, int* nums2, int nums2Size, int m) {
     
-    if(n == 0 ) return  ;
-    if(m == 0 )  {
-        for(int i = 0 ; i < n ; i++ ){
-            nums1[i] = nums2[i] ; 
+    int i = 0 ; 
+    int j = 0 ; 
+    int nums[n+m] ;  
+    while(i< n &&  j< m ){
+        if(nums1[i] < nums2[j]) nums[(i++)+j] = nums1[i];
+        else {
+            nums[i+(j++)] = nums2[j]  ;
+            
         }
-        return; 
-
-    }
-    int* nums3 = malloc((m+n)*sizeof(int) ) ; 
-
-    int ptr1 = 0; 
-    int ptr2 = 0 ;  
-    
-    for(int i = 0 ; ptr1<m || ptr2 < n ; i++){
-        if(ptr1== m ){
-            nums3[i] = nums2[ptr2] ; 
-            ptr2 ++; 
-        }
-        else if(ptr2== n ){
-            nums3[i] = nums1[ptr1] ; 
-            ptr1 ++; 
-        }
-        else if(nums1[ptr1]< nums2[ptr2]){
-            nums3[i] = nums1[ptr1] ; 
-            ptr1++; 
-        }
-        else{
-            nums3[i] = nums2[ptr2] ; 
-            ptr2 ++; 
-        }
-    }
-    for(int i = 0 ; i < nums1Size ; i ++){ 
-        nums1[i] = nums3[i]; 
-
     } 
+    while(i<n) nums[(i++)+j] = nums1[i];  
+    while(j<m ) nums[i+(j++)] = nums2[j] ; 
+
+    for(int i = 0 ; i < n+m ; i ++) {
+        nums1[i] = nums[i]; 
+    }   
+    
+    
 }
